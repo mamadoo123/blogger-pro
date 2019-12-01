71dd780d115e3fdbebf7490057d7705e759ddbb3
mohamed khamis <mkhs66662000@gmail.com>
2019-12-01 06:12:32 +0200

    added the styles to login and sign up pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    Login Page
+  <div class="row mt-4">
+    <div class="col-md-5 offset-md-3">
+      <div class="card">
+        <h3 class="card-header">Login</h3>
+        <div class="card-body">
+          <div class="form-group">
+            <input type="text" placeholder="Enetr Email" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="Enetr Password" class="form-control">
+          </div>
+          <div class="form-group text-center">
+            <button class="form-control btn btn-success">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    Sign Up Page
+  <div class="row mt-4">
+    <div class="col-md-5 offset-md-3">
+      <div class="card">
+        <h3 class="card-header">Registr Now</h3>
+        <div class="card-body">
+          <div class="form-group">
+            <input type="text" placeholder="Please Enetr Your Name" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="Enetr Email" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="Enetr Password" class="form-control">
+          </div>
+          <div class="form-group text-center">
+            <button class="form-control btn btn-success">Sign up</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>

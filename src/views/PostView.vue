6db3b38c6f719d066defc44323c5b43e7c71e45e
mohamed khamis <mkhs66662000@gmail.com>
2019-12-01 20:20:44 +0200

    added post page for single post and aded its routes and styles

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,11 +1,33 @@
 <template>
-  <h1 class="text-cenetr">{{$route.params.id}}</h1>
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 offset-md-2">
+      <div class="card" style="width: 38rem;">
+        <img :src="post.imgUrl" alt="" class="card-img-top">
+        <div class="card-body">
+          <h3 class="card-title text-center">{{post.title}}</h3>
+          <p class="card-text">Some quick example text to build on post title and make up the bulk of the post's content</p>
+          <p style="text-decoration:underline">created by: {{post.user.name}}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
+  
 </template>
 
 <script>
 import Axios from 'axios';
 
 export default {
+
+  data(){
+    return{
+      post:{}
+    }
+  },
+
   mounted(){
     this.getPost()
   },
@@ -14,7 +36,9 @@ export default {
     getPost(){
       Axios.get(`https://react-blog-api.bahdcasts.com/api/article/${this.$route.params.id}`)
             .then(response =>{
-              window.console.log(response)
+              window.console.log(response);
+              this.post = response.data.data;
+              this.post.imgUrl="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
             })
     }
   }
